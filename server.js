a364623236c99e448641dd2e5a841009f25937fe
Raghav Sai <rags.comp@gmail.com>
2016-09-18 01:06:44 +0530

    Beautified code

--- server.js
+++ server.js
@@ -69,24 +69,24 @@ function createTemplate(data) {
   return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/:articleName', function(req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log(`IMAD course app listening on port ${port}!`);
 });
