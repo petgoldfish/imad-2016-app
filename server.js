8bb63e067a4d20e9160121b4b5bd04420d6743ed
Raghav Sai <rags.comp@gmail.com>
2016-09-17 23:44:01 +0530

    Made article one

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-  res.send('Article 1 was requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res){
