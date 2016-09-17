47df06b3d357f0313e32f18a4250107e237337c7
Raghav Sai <rags.comp@gmail.com>
2016-09-18 00:42:22 +0530

    Compressed html code to use express injection

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+  'article-one': {
+    title: 'Article One! Raghav Sai',
+    heading: 'Article One',
+    date: 'Sep 5, 2016',
+    content: `
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, eligendi.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, eligendi.
+              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id cupiditate ipsam at, voluptate ex neque. Quis culpa necessitatibus numquam? Reiciendis.
+              </p>`
+  },
+  'article-two': {
+    title: 'Article Two! Raghav Sai',
+    heading: 'Article Two',
+    date: 'Sep 5, 2016',
+    content: `
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat provident, deserunt, et quaerat perspiciatis mollitia possimus! Laboriosam nobis ipsa quis quo numquam, tenetur veniam quaerat blanditiis, cupiditate dicta est earum pariatur. Optio minima at perferendis maiores consequuntur ad aut perspiciatis omnis qui, vero, autem minus illum beatae vitae eum delectus!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nesciunt nulla incidunt iusto consectetur beatae sapiente ipsum tenetur, vero numquam.</p>`
+  },
+  'article-three': {
+    title: 'Article Three! Raghav Sai',
+    heading: 'Article Three',
+    date: 'Sep 6, 2016',
+    content: `
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet id quia commodi nisi aliquam iusto architecto nemo in quis voluptatem!</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda voluptates beatae mollitia exercitationem vel ab ad et minima, fuga laborum, illum id cupiditate veniam vitae delectus nesciunt debitis magni.</p>`
+  }
+};
+
+function createTemplate(data) {
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+  var htmlTemplate = `
+  <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>${heading}</h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                  ${content}
+              </div>
+          </div>
+      </body>
+  </html>
+  `;
+  return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,16 +81,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
