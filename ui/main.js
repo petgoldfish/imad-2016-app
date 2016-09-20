67f4be30974f609b9c1fbbe8488636a34f89f9f7
Raghav Sai <rags.comp@gmail.com>
2016-09-20 22:11:56 +0530

    Did module 5

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+// change the main text element
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New Value'
+
+// change the pic to jump on mouse click
+var imgelement = document.getElementById('madi');
+var marginleft = 0;
+imgelement.onclick = function() {
+    function moveRight() {
+        marginleft = marginleft + 1;
+        imgelement.style.marginLeft = marginleft + 'px';
+    }
+    var interval = setInterval(moveRight, 50);
+}
