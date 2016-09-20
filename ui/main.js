080d3c9f6d003069e5828c694d7d25971e7dda5d
Raghav Sai <rags.comp@gmail.com>
2016-09-20 22:24:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 // change the main text element
 var element = document.getElementById('main-text');
 
-element.innerHTML = 'New Value'
+element.innerHTML = 'Click on Madi and see what happens!';
 
 // change the pic to jump on mouse click
 var imgelement = document.getElementById('madi');
@@ -14,4 +14,4 @@ imgelement.onclick = function() {
         imgelement.style.marginLeft = marginleft + 'px';
     }
     var interval = setInterval(moveRight, 50);
-}
+};
