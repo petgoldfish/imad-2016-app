ffa4c039db06b6e7340c4c8d04137841da2ee99d
Raghav Sai <rags.comp@gmail.com>
2016-09-22 19:21:55 +0530

    Another whoopsie

--- ui/main.js
+++ ui/main.js
@@ -46,6 +46,6 @@ submit.onclick = function () {
 	var nameInput = document.getElementById('name');
 	var name = nameInput.value;
 	// Make a request
-	request.open('GET', 'http://http://petgoldfish.imad.hasura-app.io/submit-name?name=' + name, true);
+	request.open('GET', 'http://petgoldfish.imad.hasura-app.io/submit-name?name=' + name, true);
 	request.send(null);
 }; 
