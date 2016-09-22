8df98ae2ddc9db0ceff15a397357a29a04da2c38
Raghav Sai <rags.comp@gmail.com>
2016-09-22 18:47:16 +0530

    Added names API

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ button.onclick = function () {
 	// Create request
 	var request = new XMLHttpRequest();
 	// Capture response into a var
-	request.onreadystatechange = function() {
-		if(request.readyState === XMLHttpRequest.DONE) {
-			if(request.status === 200) {
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
 				var counter = request.responseText;
 				var span = document.getElementById('count');
 				span.innerHTML = counter.toString();
@@ -19,3 +19,32 @@ button.onclick = function () {
 	request.open('GET', 'http://petgoldfish.imad.hasura-app.io/counter', true);
 	request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function () {
+
+	// Create request
+	var request = new XMLHttpRequest();
+	// Capture response into a var
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				// Capture a list and render
+				var names = request.responseText;
+				names = JSON.parse(names);
+				var list = '';
+				for (var i = 0; i < names.length; i++) {
+					list += '<li>' + names[i] + '</li>';
+				}
+				var ul = document.getElementById('namelist');
+				ul.innerHTML = list;
+			}
+		}
+	};
+	// Make a request
+	request.open('GET', 'http://petgoldfish.imad.hasura-app.io/submit-name?name=' + name, true);
+	request.send(null);
+}; 
