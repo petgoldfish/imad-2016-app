f400bc65a746cfc53ca22b89a8aadf98a1e5421a
Raghav Sai <g.raghav.sai@gmail.com>
2016-10-25 16:45:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/ui/madi.png', function(req, res) {
 app.get('/articles/:articleName', function(req, res) {
   var articleName = req.params.articleName;
   
-  pool.query("SELECT * from article WHERE title = " , [req.params.articleName], function(err, result){
+  pool.query("SELECT * from article WHERE title = $1" , [req.params.articleName], function(err, result){
       if(err){
           res.status(500).send(err.toString);
       } else {
